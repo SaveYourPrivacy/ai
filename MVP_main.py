e531425f56766fcdd1f372732d7b3417df3be1d7
KimKiJung8675 <wnszl8675@gmail.com>
2025-12-11 19:00:11 +0900

    [REFACTOR] 코드 수정

--- MVP_main.py
+++ MVP_main.py
@@ -6,13 +6,8 @@ from Terms_Analyze.core.MVP_rag import setup_rag
 from Terms_Analyze.routers import MVP
 from Complain_Email.routers import Complain_Email
 from Company_Terms_Analzye.routers import Company_Terms_Analzye
-<<<<<<< Updated upstream
 from ResponseExcel.routers import MVPExcel
-=======
-from AdditionalNotes.routers import AdditionalNotes
-from Improvement.routers import Improvement
 from CaseSearch.routers import CaseSearch
->>>>>>> Stashed changes
 
 app = FastAPI()
 app.add_middleware(
@@ -29,13 +24,10 @@ app.include_router(MVP.router)
 app.include_router(Complain_Email.router)
 # 기업용 약관 취약점 분석 라우터
 app.include_router(Company_Terms_Analzye.router)
-<<<<<<< Updated upstream
 # 엑셀 반환 라우터
 app.include_router(MVPExcel.router)
-=======
 #구글 검색 기능
 app.include_router(CaseSearch.router)
->>>>>>> Stashed changes
 
 # FastAPI 구동시 RAG 셋업을 최초 1회 실행
 @app.on_event("startup")
