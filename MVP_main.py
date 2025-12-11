78171ae18a941a69f06fbb557d1d76e4a5750421
KimKiJung8675 <wnszl8675@gmail.com>
2025-12-11 18:02:21 +0900

    캐시 제거

--- MVP_main.py
+++ MVP_main.py
@@ -5,7 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from Terms_Analyze.core.MVP_rag import setup_rag
 from Terms_Analyze.routers import MVP
 from Company_Terms_Analzye.routers import Company_Terms_Analzye
+<<<<<<< Updated upstream
 from AdditionalNotes.routers import AdditionalNotes
+=======
+from Improvement.routers import Improvement
+
+>>>>>>> Stashed changes
 
 app = FastAPI()
 app.add_middleware(
@@ -18,11 +23,18 @@ app.add_middleware(
 
 # MVP 약관분석 라우터
 app.include_router(MVP.router)
+<<<<<<< Updated upstream
 #추가 상황에 따른 행동 지침 라우터
 app.include_router(AdditionalNotes.router)
 # 기업용 약관 취약점 분석 라우터
 app.include_router(Company_Terms_Analzye.router)
 # 엑셀 반환 라우터
+=======
+#구체적인 약관 개선 사항 제공
+app.include_router(Improvement.router)
+# 기업용 약관 취약점 분석 라우터
+app.include_router(Company_Terms_Analzye.router)
+>>>>>>> Stashed changes
 
 # FastAPI 구동시 RAG 셋업을 최초 1회 실행
 @app.on_event("startup")
