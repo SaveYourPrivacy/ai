e746f2ebfbeaccfbba5e23de7ef1592be1018350
KimKiJung8675 <wnszl8675@gmail.com>
2025-12-14 20:15:07 +0900

    [REFACTOR] #31 메모리 기능 추가

--- Terms_Analyze/routers/MVP.py
+++ Terms_Analyze/routers/MVP.py
@@ -1,9 +1,11 @@
 from typing import List
 from fastapi import APIRouter
 import uuid
-from Terms_Analyze.schemas.MVP_dto import TermInput, TermsResponse
+from langchain.memory import ConversationBufferMemory
+from Terms_Analyze.schemas.MVP_dto import TermInput, TermsResponse, UnfairClause
 from Terms_Analyze.core.MVP_rag import get_retriever
 from Terms_Analyze.core.MVP_chain import term_chain
+from Terms_Analyze.schemas.MVP_dto import sessions
 
 router = APIRouter(
     tags=["UnfairTerm Analysis"]
@@ -36,10 +38,17 @@ def analyze(input: TermInput) -> TermsResponse:
     }
 
     session_id = str(uuid.uuid4())
+    memory = ConversationBufferMemory(return_messages=True)
 
     # LLM 호출 및 출력값 반환
     response = term_chain.invoke(chain_input) # response는 parser에 의해 생성된 딕셔너리가 초기화됨.
 
+    memory.save_context(
+        {"input":f"이것은 방금 분석된 약관의 불공정 조항들입니다. \n {response}"},
+        {"output": "네, 해당 정보를 바탕으로 컴플레인 메일 작성을 도와드리겠습니다."}
+    )
+
+    sessions[session_id] = memory
     response["session_id"] = session_id
     
     return response
